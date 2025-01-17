a9c0ee2eda69d653c95d5cafa2968cb8cade462e
xmhu2001 <2268104085@qq.com>
2025-01-17 21:52:51 +0800

    user server service

---
+++ app/user/biz/model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Email          string `gorm:"uniqueIndex"`
+	PasswordHashed string `gorm:"type:varchar(255) not null"`
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
